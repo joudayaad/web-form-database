59cc29dcd58551c2cde4f90a9ba5f3b514f50bf0
joudayaad <joud.ayaad@gmail.com>
2026-08-17 20:19:07 +0300

    Update toggle.php

--- toggle.php
+++ toggle.php
@@ -8,12 +8,12 @@ if ($conn->connect_error) {
 if (isset($_POST['id'])) {
     $id = intval($_POST['id']);
 
-    $result = $conn->query("SELECT status FROM users WHERE id = $id");
+    $result = $conn->query("SELECT status FROM user WHERE id = $id");
 
     if ($result && $row = $result->fetch_assoc()) {
         $newStatus = $row['status'] == 0 ? 1 : 0;
 
-        $conn->query("UPDATE users SET status = $newStatus WHERE id = $id");
+        $conn->query("UPDATE user SET status = $newStatus WHERE id = $id");
 
         echo $newStatus;
     }
